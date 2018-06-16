77c86651af87d420f406f41be69908608f62a16d
Devashish2910 <dgpatel2910@gmail.com>
2018-06-15 23:34:32 -0700

    :ok_hand: 5.2 Increment an Arbitrary-Precision Integer

--- 5/5.2/script.py
+++ 5/5.2/script.py
@@ -5,6 +5,8 @@ E.g -
 [1, 2, 9= => [1, 3, 0]
 """
 def increment_by_one(arr):
+    # Kids do this
+    """
     num = 0
     for digit in arr:
         num = (num * 10) + digit
@@ -19,7 +21,21 @@ def increment_by_one(arr):
         num //= 10
 
     return result
+    """
+    arr[-1] += 1
 
-print(increment_by_one([1, 2, 3]))
-print(increment_by_one([1, 2, 3, 9]))
-print(increment_by_one([9, 9]))
+    for i in range(len(arr)-1, 0, -1):
+        if arr[i] is not 10:
+            break
+        arr[i] = 0
+        arr[i-1] += 1
+
+    if arr[0] == 10:
+        arr[0] = 1
+        arr.append(0)
+
+    return arr
+
+print(f"[1, 2, 3] => {increment_by_one([1, 2, 3])}")
+print(f"[9, 2, 3] => {increment_by_one([9, 2, 3])}")
+print(f"[9, 9] => {increment_by_one([9, 9])}")
